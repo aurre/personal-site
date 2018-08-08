83dd8ddbe30f10597fb3c446e89c3097e8963339
Raysa Aurrecochea <aurrecochearaysa@gmail.com>
2018-08-07 19:37:18 -0500

    new layout

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static('.'));
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
